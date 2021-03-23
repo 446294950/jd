9e8f983bb7ed9333bea2d7fd4f99ff52fab5e02e
hocyushing <58301540+hocyushing@users.noreply.github.com>
2021-03-23 22:58:23 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -40,7 +40,7 @@ my_scripts_list_4=""
 my_scripts_list_5="format_share_jd_code.js"
 
 # 维护:ZCY01       库地址:https://github.com/ZCY01/daily_scripts/tree/main/jd
-my_scripts_list_6="jd_priceProtect.js"
+my_scripts_list_6=""
 
 # 维护:shuye72     库地址:https://gitee.com/shuye72/MyActions/tree/main ps:尽量不要使用此人脚本,
 my_scripts_list_7=""
